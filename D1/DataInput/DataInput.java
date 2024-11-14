38051cb972c45cddc3d3a7eb662acf2c4b32293f
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-14 15:59:13 +0545

    # no change

--- D1/DataInput/DataInput.java
+++ D1/DataInput/DataInput.java
@@ -6,19 +6,23 @@ public class DataInput {
     public static void main(String[] args) {
         // Input
         Scanner sc = new Scanner(System.in);
-        System.out. print(" Enter your name: ");
+
+        System.out.print("Enter your name: ");
         String name = sc.nextLine();
-        // nextLine used enput string type data
+        // nextLine used to input string type data
+
         System.out.print("Enter your age: ");
         int age = sc.nextInt();
-        // nextInt used enput int type data
+        // nextInt used to input int type data
+
         System.out.print("Enter your height: ");
         double height = sc.nextDouble();
-        // nextDouble used enput double type data
-        System.out.print("Enter your GPA");
+        // nextDouble used to input double type data
+
+        System.out.print("Enter your GPA: ");
         float GPA = sc.nextFloat();
-        // nextFloat used enput float type data
-        
+        // nextFloat used to input float type data
+
         // Output
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
