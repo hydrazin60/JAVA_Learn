3e9048fd83f2c12d20cfd9cad9e95a8c19d4272e
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-14 15:42:40 +0545

    Basic + Data Type learn

---
+++ D1/dataType/dataType.java
@@ -0,0 +1,10 @@
+package D1.dataType;
+
+public class dataType {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int ans = (a * b) / (a - b);
+        System.out.println(" Answer = " + ans);
+    }
+}
---
+++ D1/index.java
@@ -0,0 +1,15 @@
+package D1;
+
+public class index {
+    public static void main(String[] args) {
+        System.out.println(" Hello World! with Java ");
+        System.out.print(" Hello World! with Java \n"); 
+        System.out.print(" Hello World! with Java \n");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
+
