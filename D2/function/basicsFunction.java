c7368fe229ced8ee2863da400f2f7797783ea057
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-17 18:59:31 +0545

    factorial calculation function

--- D2/function/basicsFunction.java
+++ D2/function/basicsFunction.java
@@ -1,22 +1,26 @@
 package D2.function;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class basicsFunction {
+    public static void multiple(int a) {
 
-    public static int add(int a, int b) {
-        return a + b;
+        if (a < 0) {
+            System.out.println("Factorial of negative number doesn't exist");
+            return;
+        }
+        int factorial = 1;
+        for (int i = 1; i <= a; i++) {
+            factorial *= i;
+        }
+        System.out.println("Factorial of " + a + " is: " + factorial);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first number: ");
+        System.out.print("Enter a number: ");
         int a = sc.nextInt();
-        System.out.print("Enter second number: ");
-        int b = sc.nextInt();
-        int sum = add(a, b);
-
-        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+        multiple(a);
+        sc.close();
     }
-
 }
