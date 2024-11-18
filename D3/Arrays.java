3e2cbf430af049ad9fe0e260aeb19df0283468db
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-18 18:40:21 +0545

    defination (method of create ) an Array 2

--- D3/Arrays.java
+++ D3/Arrays.java
@@ -1,12 +1,19 @@
 public class Arrays {
     public static void main(String[] args) {
-        // type[] arrayName = new type[size];
-        int marks[] = new int[5];
-        marks[0] = 10;
-        marks[1] = 20;
-        marks[2] = 30;
-        marks[3] = 40;
-        marks[4] = 50;
+        // // type[] arrayName = new type[size];
+        // int marks[] = new int[5];
+        // marks[0] = 10;
+        // marks[1] = 20;
+        // marks[2] = 30;
+        // marks[3] = 40;
+        // marks[4] = 50;
+        // for (int i = 0; i < marks.length; i++) {
+        // System.out.println(marks[i]);
+        // }
+
+        // ############# defination an array 2
+        // type[] arrayName = {value1, value2, value3, value4, value5};
+        int marks[] = { 10, 20, 30, 40, 50 };
         for (int i = 0; i < marks.length; i++) {
             System.out.println(marks[i]);
         }
@@ -14,4 +21,3 @@ public class Arrays {
     }
 
 }
- 
