3ea280173fbb9c0ee0e7ed04b4d07137b3128b21
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-18 18:33:32 +0545

    basic Array in Java

---
+++ D3/Arrays.java
@@ -0,0 +1,17 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // type[] arrayName = new type[size];
+        int marks[] = new int[5];
+        marks[0] = 10;
+        marks[1] = 20;
+        marks[2] = 30;
+        marks[3] = 40;
+        marks[4] = 50;
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+
+    }
+
+}
+ 
