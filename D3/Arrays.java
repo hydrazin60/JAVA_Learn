9f4f59821461924cb1fd199aa74a40279bc5b5cd
hydrazin60 <sureshkhdk45@gmail.com>
2024-11-19 15:10:56 +0545

    best example of Array program

--- D3/Arrays.java
+++ D3/Arrays.java
@@ -1,23 +1,19 @@
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String[] args) {
-        // // type[] arrayName = new type[size];
-        // int marks[] = new int[5];
-        // marks[0] = 10;
-        // marks[1] = 20;
-        // marks[2] = 30;
-        // marks[3] = 40;
-        // marks[4] = 50;
-        // for (int i = 0; i < marks.length; i++) {
-        // System.out.println(marks[i]);
-        // }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = sc.nextInt();
 
-        // ############# defination an array 2
-        // type[] arrayName = {value1, value2, value3, value4, value5};
-        int marks[] = { 10, 20, 30, 40, 50 };
-        for (int i = 0; i < marks.length; i++) {
-            System.out.println(marks[i]);
+        int marks[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter the marks of student " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
         }
-
+        for (int i = 0; i < size; i++) {
+            System.out.println("Marks of student " + (i + 1) + ": " + marks[i]);
+        }
     }
 
 }
